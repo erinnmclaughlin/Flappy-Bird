519ec5acdffdf88bfa849a4f44e88db2d4cf0328
erinnmclaughlin <erinmclaughlin219@gmail.com>
2020-08-13 19:38:18 -0400

    Give bird the ability to jump

--- src/FlappyBird.Web/Models/BirdModel.cs
+++ src/FlappyBird.Web/Models/BirdModel.cs
@@ -3,10 +3,17 @@
     public class BirdModel
     {
         public int DistanceFromBottom { get; private set; } = 100;
+        public int JumpStrength { get; private set; } = 50;
 
         public void Fall(int gravity)
         {
             DistanceFromBottom -= gravity;
+        }
+
+        public void Jump()
+        {
+            if (DistanceFromBottom < 530)
+                DistanceFromBottom += JumpStrength;
         }
     }
 }
