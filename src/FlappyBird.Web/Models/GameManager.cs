9f481a6162f9da03c585f33952ea3165611479e0
erinnmclaughlin <erinmclaughlin219@gmail.com>
2020-08-15 07:13:38 -0400

    Use ObservableCollection to hold pipe objects

--- src/FlappyBird.Web/Models/GameManager.cs
+++ src/FlappyBird.Web/Models/GameManager.cs
@@ -1,22 +1,27 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace FlappyBird.Web.Models
 {
-    public class GameManager
+    public class GameManager : INotifyPropertyChanged
     {
         private readonly int _gravity = 2;
         private readonly int _speed = 2;
 
+        public event PropertyChangedEventHandler PropertyChanged;
+
         public bool IsRunning { get; private set; } = false;
 
         public BirdModel Bird { get; private set; }
-        public PipeModel Pipe { get; private set; }
+        public ObservableCollection<PipeModel> Pipes { get; private set; }
 
         public GameManager()
         {
             Bird = new BirdModel();
-            Pipe = new PipeModel();
+            Pipes = new ObservableCollection<PipeModel>();
+            Pipes.CollectionChanged += (o, e) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(Pipes)));
         }
 
         public async void MainLoop()
@@ -26,13 +31,27 @@ namespace FlappyBird.Web.Models
             while(IsRunning)
             {
                 Bird.Fall(_gravity);
-                Pipe.Move(_speed);
+                foreach (var pipe in Pipes)
+                {
+                    pipe.Move(_speed);
+                }
 
                 if (Bird.DistanceFromBottom <= 0)
                     GameOver();
 
+                if (!Pipes.Any() || Pipes.Last().DistanceFromLeft < 250)
+                    GeneratePipe();
+
+                if (Pipes.First().DistanceFromLeft < -60)
+                    Pipes.Remove(Pipes.First());
+
                 await Task.Delay(20);
             }
+        }
+
+        private void GeneratePipe()
+        {
+            Pipes.Add(new PipeModel());
         }
 
         private void GameOver()
