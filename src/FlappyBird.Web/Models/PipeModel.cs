69cdc62b1ed20bf0feaa115927c231d14eee22a5
erinnmclaughlin <erinmclaughlin219@gmail.com>
2020-08-24 23:06:18 -0400

    Clean up pipemodel class

--- src/FlappyBird.Web/Models/PipeModel.cs
+++ src/FlappyBird.Web/Models/PipeModel.cs
@@ -4,17 +4,12 @@ namespace FlappyBird.Web.Models
 {
     public class PipeModel
     {
-        private int _speed = 2;
+        private readonly int _speed = 2;
 
         public int DistanceFromLeft { get; private set; } = 500;
         public int DistanceFromBottom { get; private set; } = new Random().Next(1, 60);
-        
-        // pipe height - ground height + pipe distance from bottom
-        public int GapLower => 300 - 150 + DistanceFromBottom;
-
-        // pipe gap - ground height + pipe distance from bottom
-        public int GapUpper => 430 - 150 + DistanceFromBottom;
-
+        public int GapLower => 300 - 150 + DistanceFromBottom; // pipe height - ground height + pipe distance from bottom
+        public int GapUpper => 430 - 150 + DistanceFromBottom; // pipe gap - ground height + pipe distance from bottom
 
         public void Move()
         {
@@ -24,12 +19,12 @@ namespace FlappyBird.Web.Models
         public bool IsCentered()
         {
             // half of the game width minus half the width of the bird
-            var centerLeft = (500 - 60) / 2;
+            var gameCenterLeft = (500 - 60) / 2;
 
             // half of the game width plus half the width of the bird
-            var centerRight = (500 + 60) / 2;            
+            var gameCenterRight = (500 + 60) / 2;            
 
-            return (DistanceFromLeft < centerRight && DistanceFromLeft > centerLeft - 60);
+            return (DistanceFromLeft < gameCenterRight && DistanceFromLeft > gameCenterLeft - 60); 
         }
     }
 }
